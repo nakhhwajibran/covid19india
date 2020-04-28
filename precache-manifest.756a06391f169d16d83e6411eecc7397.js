6f0818c325c0af2ece84309dabe642490cac7779
nakhhwajibran <nakhwa.jibrann@gmail.com>
2020-04-28 15:43:24 +0400

    Updates

---
+++ precache-manifest.756a06391f169d16d83e6411eecc7397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f4d221424cd222bf1d4b3b267fd0ee",
+    "url": "/covid19india/index.html"
+  },
+  {
+    "revision": "3b589e61270380619c9f",
+    "url": "/covid19india/static/css/main.5b6cb991.chunk.css"
+  },
+  {
+    "revision": "353f462b60f2da263b54",
+    "url": "/covid19india/static/js/2.4a725d86.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19india/static/js/2.4a725d86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b589e61270380619c9f",
+    "url": "/covid19india/static/js/main.bec43d30.chunk.js"
+  },
+  {
+    "revision": "5c993b3a5743d9542c6f",
+    "url": "/covid19india/static/js/runtime-main.74a9a3b3.js"
+  }
+]);
